49ceb25902dc415889d8e1453bb32a15411d10c1
David McGinnis <david.mcginnis@bl-mc.se>
2022-01-14 12:58:14 +0100

    implemented blinky-bus

--- pm2.sh
+++ pm2.sh
@@ -4,5 +4,5 @@ cd $*
 while read line; do
     export $line
 done < .env
-pm2 start node_modules/node-red/red.js --node-args="--max-old-space-size=384" --restart-delay 10000 -- -s ./settings.js -u ./
+pm2 --name $PM2NAME start node_modules/node-red/red.js --node-args="--max-old-space-size=384" --restart-delay 10000 -- -s ./settings.js -u ./
 
